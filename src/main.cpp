381c4a7105bdce551e5b99e2b775af6b4dc9c49c
H-Maxence <maxence.heroux@edu.ece.fr>
2025-04-30 14:51:24 +0200

    Blink2Test

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,16 @@
 
 void setup() {
   pinMode(25, OUTPUT);  // Définit GPIO 25 comme une sortie
+  pinMode(26, OUTPUT);  // Définit GPIO 25 comme une sortie
 }
 
 void loop() {
   digitalWrite(25, HIGH);  // Allume la LED
+  digitalWrite(26, HIGH);  // Allume la LED
   delay(500);              // Attend 500 ms
   digitalWrite(25, LOW);   // Éteint la LED
+  digitalWrite(26, LOW);   // Éteint la LED
   delay(500);              // Attend 500 ms
 }
+
+
