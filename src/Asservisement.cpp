bfed375ab65f5c3fab69aeafe81da2ca97a6d59d
H-Maxence <maxence.heroux@edu.ece.fr>
2025-05-13 18:55:41 +0200

    PID polaire

--- src/Asservisement.cpp
+++ src/Asservisement.cpp
@@ -10,56 +10,52 @@ int Ticks_to_Distance (int distance){
     return (int)ticks;
 }
 
-int erreur_dist_D, erreur_dist_G;
-int I_erreur_dist_D, I_erreur_dist_G;
-int D_erreur_dist_D, D_erreur_dist_G;
-int erreur_dist_D_prec, erreur_dist_G_prec;
-int commande_ticks_MD, commande_ticks_MG;
+int erreur_dist_D, erreur_dist_G,erreur_dist;
+int I_erreur_dist;
+int D_erreur_dist;
+int erreur_dist_prec;
+int commande_ticks;
 int commande_pwm_dist_MD, commande_pwm_dist_MG;
 
 int Avancer (int dist_consigne, float KP, float KI, float KD){ //TODO  ajouter une rampe 28:06 min
     //erreurs
     erreur_dist_D = Ticks_to_Distance(dist_consigne) - nb_tic_encodeur_D; 
     erreur_dist_G = Ticks_to_Distance(dist_consigne) - nb_tic_encodeur_G; 
+    erreur_dist = (erreur_dist_D-erreur_dist_G)/2;
     //erreur i et d
-    I_erreur_dist_D += erreur_dist_D;
-    I_erreur_dist_G += erreur_dist_G; //TODO anti wind up
-    D_erreur_dist_D =  erreur_dist_D - erreur_dist_D_prec;
-    D_erreur_dist_G =  erreur_dist_G - erreur_dist_G_prec; 
-    erreur_dist_D_prec = erreur_dist_D;
-    erreur_dist_G_prec = erreur_dist_G;
+    I_erreur_dist += erreur_dist;
+    D_erreur_dist =  erreur_dist - erreur_dist_prec;
+    erreur_dist_prec = erreur_dist;
     //pid
-    commande_ticks_MD = KP * erreur_dist_D + KI * I_erreur_dist_D + KD * D_erreur_dist_D;
-    commande_ticks_MG = KP * erreur_dist_G + KI * I_erreur_dist_G + KD * D_erreur_dist_G;
-
+    commande_ticks = KP * erreur_dist_D + KI * I_erreur_dist + KD * D_erreur_dist;
+    
     //cap la valeur max a la vitesse max
-    commande_pwm_dist_MD = constrain(abs(commande_ticks_MD), LOWEST_PWM, HIGHTEST_PWM) * signe(commande_ticks_MD);
-    commande_pwm_dist_MG = constrain(abs(commande_ticks_MG), LOWEST_PWM, HIGHTEST_PWM) * signe(commande_ticks_MG);  
+    commande_pwm_dist_MD = constrain(abs(commande_ticks), 0, HIGHTEST_PWM) * signe(commande_ticks);
+    commande_pwm_dist_MG = constrain(abs(commande_ticks), 0, HIGHTEST_PWM) * signe(commande_ticks);  
 
     //TODO kick start
 
     //mouvement fini ?
-    if (abs(erreur_dist_D) < 20 && abs(erreur_dist_G) < 20) {
-        //PWM a 0
-        commande_pwm_dist_MD =0;
-        commande_pwm_dist_MG =0;
-        //reset erreur 
-        //Reset_pid_distance();
-        return 1;
-    } else {
-        return 0;
-    }
+    // if (abs(erreur_dist_D) < 20 && abs(erreur_dist_G) < 20) {
+    //     //PWM a 0
+    //     commande_pwm_dist_MD =0;
+    //     commande_pwm_dist_MG =0;
+    //     //reset erreur 
+    //     //Reset_pid_distance();
+    //     return 1;
+    // } else {
+    //     return 0;
+    // }
+    return 0;
 }
 
 void Reset_pid_distance (void){
     erreur_dist_D =0;
     erreur_dist_G =0;
-    I_erreur_dist_D =0;
-    I_erreur_dist_G =0; 
-    D_erreur_dist_D =0;
-    D_erreur_dist_G =0; 
-    erreur_dist_D_prec =0;
-    erreur_dist_G_prec =0;
+    erreur_dist =0;
+    I_erreur_dist =0; 
+    D_erreur_dist =0;
+    erreur_dist_prec =0;
 }
 
 void DEBUG_PID_distance (int consigne_dist){
@@ -127,21 +123,22 @@ int Tourner (int angle, float KP, float KI, float KD){
     commande_rot = KP * erreur_rot + KI * I_erreur_rot + KD * D_erreur_rot;
     
     //cap la valeur max a la vitesse max
-    commande_pwm_angle_MD = constrain(abs(commande_rot), LOWEST_PWM, HIGHTEST_PWM) * signe(commande_rot); 
+    commande_pwm_angle_MD = constrain(abs(commande_rot), 0, HIGHTEST_PWM) * signe(commande_rot); 
     commande_pwm_angle_MG = - commande_pwm_angle_MD;
     //TODO kick start
 
     //mouvement fini ?
-    if (abs(erreur_rot) < 1) {
-        //PWM a 0
-        commande_pwm_angle_MD =0;
-        commande_pwm_angle_MG =0;
-        //reset erreur 
-        //Reset_pid_angle();
-        return 1;
-    } else {
-        return 0;
-    }
+    // if (abs(erreur_rot) < 1) {
+    //     //PWM a 0
+    //     commande_pwm_angle_MD =0;
+    //     commande_pwm_angle_MG =0;
+    //     //reset erreur 
+    //     //Reset_pid_angle();
+    //     return 1;
+    // } else {
+    //     return 0;
+    // }
+    return 0;
 }
 
 void Reset_pid_angle(void){
--- src/main.cpp
+++ src/main.cpp
@@ -33,20 +33,25 @@ void loop() {
   int Commande_MD =0, Commande_MG =0;
   
   // TODO XY vers arg et module + In cercle/zone + Postion absolue MAJ
-  consigne_dist = 0; 
-  consigne_angle = 90; // sans trigo
-
-  if(!CURVILIGNE && abs(Angle_restriction(consigne_angle *PI/180) - anglerobot)>(3 * PI/180)){ //5 degres de tolerance
-    commande_pwm_dist_MD =0;
-    commande_pwm_dist_MG =0;
-  }
+  consigne_dist = 1000; 
+  consigne_angle = -30; // sans trigo
 
   //FIXME : regler coeff et caper les pid I
-  Tourner(consigne_angle, 1, 1.6, 0);
-  Avancer(consigne_dist, 0.2, 0, 0); //Pas fair ca faire une XY en cercle et calculer angle et distance pa rapport a la position actuelle en live , pas de fin de pid, qunad robot dans le cercle => changement de position 
+  Tourner(consigne_angle, 1.2, 0, 0); // 1.2, 0.5, 0
+  Avancer(consigne_dist, 0.2, 0, 0); //  0.2, 0, 0 Pas fair ca faire une XY en cercle et calculer angle et distance pa rapport a la position actuelle en live , pas de fin de pid, qunad robot dans le cercle => changement de position 
+
+  // if(!CURVILIGNE && abs(Angle_restriction(consigne_angle *PI/180) - anglerobot)>(3 * PI/180)){ //5 degres de tolerance
+  //   commande_pwm_dist_MD =0;
+  //   commande_pwm_dist_MG =0;
+  // }
 
   Commande_MD = constrain(commande_pwm_dist_MD + commande_pwm_angle_MD, -HIGHTEST_PWM, HIGHTEST_PWM); //rotation positive
   Commande_MG = constrain(commande_pwm_dist_MG + commande_pwm_angle_MG, -HIGHTEST_PWM, HIGHTEST_PWM); //rotation negative
+
+  Serial.print(">commande_pwm_angle_MD:");
+  Serial.print(commande_pwm_angle_MD);
+  Serial.print(">commande_pwm_angle_MG:");
+  Serial.print(commande_pwm_angle_MG);
 
   PWM('D',Commande_MD);
   PWM('G',Commande_MG);
